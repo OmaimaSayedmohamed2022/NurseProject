4ae9b08f4a0913048deb4650fc9c08b5cf3dec1e
Your Name <you@example.com>
2025-03-11 06:35:33 +0200

    auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 const  router= express.Router()
-import { loginUser, logOutUser } from "../controllers/authController.js";
+import {saveFingerprint, loginUser, logOutUser } from "../controllers/authController.js";
+// import { resetPassword } from "../controllers/resetPasswordController.js";
 
 
 router.post('/login',loginUser)
 router.post('/logout',logOutUser)
+
+router.post('/fingerPrint',saveFingerprint)
+
+// forgetPassword
+// router.post('/resetPassword', resetPassword);
 
 export default router;
