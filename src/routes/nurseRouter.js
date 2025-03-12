a5c2e40f487cd9c7e9d65a51670d4ed89c7f487e
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2025-03-12 07:30:50 +0200

    Update nurseRouter.js

--- src/routes/nurseRouter.js
+++ src/routes/nurseRouter.js
@@ -3,6 +3,7 @@ import { register, getAllNurses, getNurseById, updateNurse, deleteNurse } from '
 import upload from '../middlewares/uploadImage.js';
 import {nurseValidation} from "../validations/nurseValidation.js"
 import nurseMiddleware from '../middlewares/nurseMiddleware.js';
+import { contactUs } from '../controllers/contactController.js';
 
 const router = express.Router();
 
@@ -14,8 +15,10 @@ router.get('/getNurse/:nurseId', getNurseById);
 router.patch('/update/:nurseId', updateNurse);
 router.delete('/delete/:nurseId', deleteNurse);
 
+//contact
+router.post("/contactUs",contactUs)
+
 
 
 
 export default router;
-
