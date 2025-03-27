70a5e8828c5353a2fad9b408d0c07dec54109dc9
Your Name <you@example.com>
2025-03-27 07:56:23 +0200

    update

--- src/models/patientDataModel.js
+++ src/models/patientDataModel.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const patientSchema = new mongoose.Schema({
   client: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true, },
   name: { type: String, required: true },
-  code: { type: String, required: true, unique: true },
+  code: { type: String, required: true },
   services: [{ type: mongoose.Schema.Types.ObjectId, ref: "Service" }],
   description: { type: String },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: String },
   date: { type: Date, required: true },
-  videoandPicture: { type: String }, 
+  videoOrPhotos: { type: String }, 
 }, { timestamps: true });
 
 const PatientData = mongoose.model("PatientData", patientSchema);
