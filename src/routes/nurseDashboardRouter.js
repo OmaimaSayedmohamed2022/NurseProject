88ad1379b7f2a155ba161efea43827e451e1a32e
ahmedwagdy-ai <ahmedwagdy_official@outlook.com>
2025-04-06 19:31:15 +0200

    nurseDashboard

---
+++ src/routes/nurseDashboardRouter.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { search, getServicesCount, getAllNurses, getPatientsForNurse,
+    getNurseEarnings,
+    getNurseSessions,
+    getRecentVisits,
+    getNurseReviews} from '../controllers/nurseDashboardController.js';
+import nurseMiddleware from '../middlewares/nurseMiddleware.js';
+
+const router = express.Router();
+
+router.get('/search', search);
+
+router.get('/servicesCount', getServicesCount);
+
+router.get('/nurses', getAllNurses);
+
+router.get('/nursePatients/:nurseId', nurseMiddleware, getPatientsForNurse);
+
+router.get('/nurseEarnings/:nurseId', nurseMiddleware, getNurseEarnings);
+
+router.get('/nurseSessions/:nurseId', nurseMiddleware, getNurseSessions);
+
+router.get('/nurseRecentVisits/:nurseId', nurseMiddleware, getRecentVisits);
+
+router.get('/nurseReviews/:nurseId', nurseMiddleware, getNurseReviews);
+
+
+export default router;
