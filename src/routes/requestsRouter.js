93c099acb02cf22203e8d0d12e22c5799f6d4b06
Your Name <you@example.com>
2025-04-17 03:24:24 +0200

    permissions done

--- src/routes/requestsRouter.js
+++ src/routes/requestsRouter.js
@@ -1,15 +1,15 @@
-import express from 'express'
-import  { getAllRequests, getRequestById,
-          createRequest, updateRequest, 
-          deleteRequest } from '../controllers/requestsController.js';
+// import express from 'express'
+// import  { getAllRequests, getRequestById,
+//           createRequest, updateRequest, 
+//           deleteRequest } from '../controllers/requestsController.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/getAll', getAllRequests);
-router.get('/getById/:id', getRequestById);
-router.post('/create', createRequest);
-router.put('/update/:id', updateRequest);
-router.delete('/delete/:id', deleteRequest);
+// router.get('/getAll', getAllRequests);
+// router.get('/getById/:id', getRequestById);
+// router.post('/create', createRequest);
+// router.put('/update/:id', updateRequest);
+// router.delete('/delete/:id', deleteRequest);
 
 
-export default router;
+// export default router;
