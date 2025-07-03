643c51294a67e59fc8a7700706cd512beca80b5c
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2025-07-03 22:06:39 +0300

    Update mongoDb.js

--- src/db/mongoDb.js
+++ src/db/mongoDb.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
     // console.log('MONGO_URI:', process.env.MONGO_URI);
       await mongoose.connect(process.env.MONGO_URI, { 
         useNewUrlParser: true,
-        useUnifiedTopology: true 
+        useUnifiedTopology: true ,
+         autoIndex: true
         });
      
       logger.info('MongoDB connected successfully');
